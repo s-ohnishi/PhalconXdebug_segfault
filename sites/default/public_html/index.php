640f13a3de7e84718b2f6a3c6852bc0252cb59af
s-ohnishi <s-ohnishi@xqt.jp>
2019-10-12 16:09:55 +0900

    segmentation fault

--- sites/default/public_html/index.php
+++ sites/default/public_html/index.php
@@ -1,1 +1,34 @@
 <?php
+
+define('APP_DIR', dirname(__DIR__) . '/app/');
+
+$settings = [
+    'database' => [
+        'adapter'     => 'Mysql',
+        'host'        => 'localhost',
+        'username'    => 'root',
+        'password'    => '',
+        'dbname'      => 'test',
+    ],
+    'application' => [
+        'controllersDir' => APP_DIR . 'controllers/',
+        'modelsDir'      => APP_DIR . 'models/',
+        'viewsDir'       => APP_DIR . 'views/',
+        'pluginsDir'     => APP_DIR . 'plugins/',
+        'libraryDir'     => APP_DIR . 'library/',
+        'cacheDir'       => APP_DIR . 'cache/',
+        'baseUri'        => '/tutorial/',
+    ]
+];
+
+$config = new Phalcon\Config($settings);
+
+// オートローダにディレクトリを登録する
+$loader = new Phalcon\Loader();
+$loader->registerDirs(
+    [
+        "../app/controllers/",
+        "../app/models/",
+    ]
+);
+
